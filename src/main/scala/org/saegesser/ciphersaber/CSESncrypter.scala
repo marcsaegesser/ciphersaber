42d26ae5d472e1c4c4edd5aef33e4fd4ceb3f573
Marc Saegesser <marc@msaegesser.(none)>
2012-12-17 12:24:40 -0600

    Removed unused file.

--- src/main/scala/org/saegesser/ciphersaber/CSESncrypter.scala
+++
@@ -1,8 +0,0 @@
-package org.saegesser.ciphersaber
-
-import java.io.{InputStream, OutputStream}
-
-trait CSEncryptor {
-  def encrypt(in: InputStream, out: OutputStream, key: Array[Byte], n: Int): Unit
-  def decrypt(in: InputStream, out: OutputStream, key: Array[Byte], n: Int): Unit
-}
